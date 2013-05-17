c0992b1465bb20f71375b66ab72564b4f1d3a105
Daemian Mack <daemianmack@gmail.com>
2013-05-16 22:19:15 -0400

    Rename approval namespace to acceptance.

---
+++ src/dirt_magnet/acceptance.clj
@@ -0,0 +1,13 @@
+(ns dirt-magnet.acceptance)
+
+(defn link-acceptable? [{:keys [params]}]
+  (println "link-acceptable? is considering a POST with params " params)
+  (= "professor-falken" (:password params)))
+
+(defn link-accepted [creation-result request]
+  (println "+++ Accepted a POST for params " (:params request))
+  creation-result)
+
+(defn link-rejected [request]
+  (println "xxx Rejected a POST for request " request)
+  "Nope!")
--- src/dirt_magnet/approval.clj
+++
@@ -1,9 +0,0 @@
-(ns dirt-magnet.approval)
-
-(defn creation-approved? [{:keys [params]}]
-  (println "Approved a POST for params " params)
-  (= "professor-falken" (:password params)))
-
-(defn creation-denied [request]
-  (println "Denied a POST for request " request)
-  "Nope!")
