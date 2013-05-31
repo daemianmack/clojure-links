d734ed8af5b428d110d2a904e9ac9b69f8f40c18
Daemian Mack <daemianmack@gmail.com>
2013-05-31 07:00:43 -0400

    Nix commentary.

--- src/dirt_magnet/service.clj
+++ src/dirt_magnet/service.clj
@@ -39,31 +39,10 @@
      ^:interceptors [(body-params/body-params) middlewares/keyword-params]
      ["/links" {:post create-link}]]]])
 
-;; You can use this fn or a per-request fn via io.pedestal.service.http.route/url-for
 (def url-for (route/url-for-routes routes))
 
-;; Consumed by dirt-magnet.server/create-server
 (def service {:env :prod
-              ;; You can bring your own non-default interceptors. Make
-              ;; sure you include routing and set it up right for
-              ;; dev-mode. If you do, many other keys for configuring
-              ;; default interceptors will be ignored.
-              ;; :bootstrap/interceptors []
               ::bootstrap/routes routes
-
-              ;; Uncomment next line to enable CORS support, add
-              ;; string(s) specifying scheme, host and port for
-              ;; allowed source(s):
-              ;;
-              ;; "http://localhost:8080"
-              ;;
-              ;;::boostrap/allowed-origins ["scheme://host:port"]
-
-              ;; Root for resource interceptor that is available by default.
               ::bootstrap/resource-path "/public"
-
-              ;; Either :jetty or :tomcat (see comments in project.clj
-              ;; to enable Tomcat)
-              ;;::bootstrap/host "localhost"
               ::bootstrap/type :jetty
               ::bootstrap/port 8080})
